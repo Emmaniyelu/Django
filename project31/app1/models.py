42285f6e0dee20885ffdad95de07736fba71534b
Emmaniyelu <137369623+Emmaniyelu@users.noreply.github.com>
2025-04-29 12:09:43 +0530

    Add files via upload

---
+++ project31/app1/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+# Create your models here.
+class Users(models.Model):
+    name=models.CharField(max_length=20,blank=False)
+    uname=models.CharField(max_length=15,primary_key=True)
+    password=models.CharField(max_length=15,blank=False)

2441fa9c14134eb09afbb94796af8ca1ad56b1b4
Emmaniyelu <137369623+Emmaniyelu@users.noreply.github.com>
2025-04-29 12:05:37 +0530

    Add files via upload

---
+++ project1/app1/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.views.generic import ListView,UpdateView,CreateView,DeleteView,TemplateView
+from app1.models import Product
+from django.urls import reverse_lazy
+from app1.forms import ProductForm
+# Create your views here.
+
+
+class ProductListView(ListView):
+    model=Product
+    fields="__all__"
+    template_name="app1/prodlist_temp.html"
+    context_object_name="products"
+
+
+class IndexTemplateView(TemplateView):
+    template_name="app1/index.html"
+
+
+class CreateProductView(CreateView):
+    model=Product 
+    template_name="app1/createprod_temp.html"
+    success_url=reverse_lazy("prod_list")
+    form_class=ProductForm
